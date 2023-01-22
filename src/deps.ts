227a78e35cedd93a93e37fcc13ba5e1a9b78a931
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-01-23 03:33:03 +0530

    test

--- src/deps.ts
+++ src/deps.ts
@@ -1,14 +1,14 @@
 /* -------------- deno standard module -------------- */
 export { EventEmitter } from 'https://deno.land/std@0.173.0/node/events.ts';
 export { serve } from 'https://deno.land/std@0.173.0/http/server.ts'; /* HTTP SERVER */
-export { load } from 'https://deno.land/std@0.173.0/dotenv/mod.ts'; /* DOTENV */
-export * as Logger from 'https://deno.land/std@0.173.0/log/mod.ts'; /* LOGGER */
+// export { load } from 'https://deno.land/std@0.173.0/dotenv/mod.ts'; /* DOTENV */
+// export * as Logger from 'https://deno.land/std@0.173.0/log/mod.ts'; /* LOGGER */
 
 /* ------------- deno third party module ------------- */
 
 /* ----------------------- npm:CDN ----------------------- */
-export { Redis } from 'https://esm.sh/ioredis@5.2.5'; /* IOREDIS */
-export type { RedisOptions } from 'https://esm.sh/ioredis@5.2.5';
+// export { Redis } from 'https://esm.sh/ioredis@5.2.5'; /* IOREDIS */
+// export type { RedisOptions } from 'https://esm.sh/ioredis@5.2.5';
 //
 // import * as redlock from 'https://esm.sh/redlock@5.0.0-beta.2'; /* REDLOCK */
 // export const Redlock = redlock.default;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { serve } from './deps.ts';
-import './global/global.ts';
-await redis.initialize();
+// import './global/global.ts';
+// await redis.initialize();
+
+import { connect } from 'https://deno.land/x/redis@v0.29.0/mod.ts';
+const redis = await connect({
+	hostname: 'redis-18015.c301.ap-south-1-1.ec2.cloud.redislabs.com',
+	port: 18015,
+	password: 'k39y54ZB6vf6ne4lMFrR3dW6vt58vBWD',
+});
+
+const ok = await redis.set('hoge', 'fuga');
+const fuga = await redis.get('hoge');
 
 serve(async () => {
-	const x = await redis.client.get('a');
+	const x = await redis.get('a');
 	return new Response('pong: ' + x);
 });
